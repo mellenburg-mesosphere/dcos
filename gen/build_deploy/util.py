e8735a34848a2a6fd5e8c8095a68a558a2a47a9b
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-03-09 13:53:25 -0800

    Patch gen

--- gen/build_deploy/util.py
+++ gen/build_deploy/util.py
@@ -9,7 +9,10 @@ from pkgpanda.util import write_json, write_string
 dcos_image_commit = os.getenv('DCOS_IMAGE_COMMIT', None)
 
 if dcos_image_commit is None:
-    dcos_image_commit = check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+    try:
+        dcos_image_commit = check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+    except:
+        pass
 
 if dcos_image_commit is None:
     raise "Unable to set dcos_image_commit from teamcity or git."
